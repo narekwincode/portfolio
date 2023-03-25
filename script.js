17c59b0f4e670cd1eaa3c51f84e7642dc23eb3fb
Narek <112431668+narekwincode@users.noreply.github.com>
2023-03-26 01:17:28 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,67 @@
+// ========================toggle icon navbat================
+
+let menuIcon=document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+menuIcon.onclick=()=>{
+    menuIcon.classList.toggle("bx-x")
+    navbar.classList.toggle("active")
+
+}
+
+
+
+
+
+
+let section=document.querySelectorAll('section')
+let navLinks=document.querySelectorAll('header nav a')
+
+window.onscroll=()=>{
+    section.forEach(sec=>{
+        let top=window.scrollY;
+        let offset=sec.offsetTop-150;
+        let height=sec.offsetHeight;
+        let id=sec.getAttribute('id')
+
+        if(yop>=offset&&top<offset+height){
+            navLinks.forEach(link=>{
+                navLinks.classList.remove('active');
+                document.querySelector('header nav a[href*='+id+']').classList.add("active")
+            })
+        }
+    });
+    // navbar sticky
+    let header=document.querySelector('header')
+    header.classList.toggle('sticky',window.scrollY>100);
+// =================================remove toggle icon WheelEvent(scroll)====================================
+    menuIcon.classList.toggle("bx-x")
+    navbar.classList.toggle("active")
+
+}
+
+
+    // ==========scroll reveal======================
+ScrollReveal({
+    // reset:true,
+    distance:'80px',
+    duration:2000,
+    delay:200
+})
+ScrollReveal().reveal(".home-content,.heading",{origin:"top"})
+ScrollReveal().reveal(".home-img,.services-container,.portfolio-box,.contact form",{origin:"bottom"})
+ScrollReveal().reveal(".home-content,h1 .about-img",{origin:"left"})
+ScrollReveal().reveal(".home-content,p .about-content",{origin:"right"})
+
+
+
+
+// =============================typesd==========================================
+const typed =new Typed('.multiple-text',{
+    strings:['Fronted Developer','YouTube','Blogger'],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+
+})
